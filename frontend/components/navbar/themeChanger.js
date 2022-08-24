c9ccd1c3fb692779a3f91d7df37778413a5aef82
musthaq96 <mushtaq96smb@gmail.com>
2022-08-24 15:15:00 +0200

    test theme change on deploy

--- frontend/components/navbar/themeChanger.js
+++ frontend/components/navbar/themeChanger.js
@@ -3,12 +3,22 @@ import { Button, StyledButtonIcon } from '@nextui-org/react';
 import { HeartIcon } from '../../icons/HeartIcon';
 import { SunIcon } from '../../icons/SunIcon';
 import { MoonIcon } from '../../icons/MoonIcon';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const ThemeChanger = () => {
     const { theme, setTheme } = useTheme();
-    // false == darkTheme
-    //const [isLight, setIsLight ] = useState(true);
+    console.log({theme})
+    const [mounted, setMounted] = useState(false)
+    console.log({mounted})
+
+    // useEffect only runs on the client, so now we can safely show the UI
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null
+      }
 
     const toggle = () => {
         if (theme == 'light') {
@@ -20,6 +30,7 @@ export const ThemeChanger = () => {
             //setIsLight(true)
         }
     }
+
     return (
         <div>
             <Button
