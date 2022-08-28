94d6ef9fec69ceea2e4505a11634720e96cdb7b1
John Agbanusi <agbanusijohn@gmail.com>
2022-08-28 17:37:36 +0100

    fix .env config and replace hardcoded value with env values

--- frontend/components/modals/transferNFTModal.jsx
+++ frontend/components/modals/transferNFTModal.jsx
@@ -1,75 +1,70 @@
-import { Button, Text, Modal, Input, Grid } from '@nextui-org/react'
-import { useState } from 'react';
+import { Button, Text, Modal, Input, Grid } from "@nextui-org/react";
+import { useState } from "react";
 
-import { useContract } from '@thirdweb-dev/react';
+import { useContract } from "@thirdweb-dev/react";
 
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 export default function TransferNFTModal() {
+  const router = useRouter();
+  const { contractAddress } = router.query;
+  const { nftId } = router.query;
 
-    const router = useRouter()
-    const { contractAddress } = router.query;
-    const { nftId } = router.query;
+  const [visible, setVisible] = useState(false);
+  const [recipientAddress, setRecipientAddress] = useState(process.env.OWNER);
 
-    const [visible, setVisible] = useState(false);
-    const [recipientAddress, setRecipientAddress] = useState("0x954184AD0Fbc67332Bab62a6c5958c4C5E2CFeC2")
+  const handler = () => setVisible(true);
+  const closeHandler = () => setVisible(false);
 
-    const handler = () => setVisible(true);
-    const closeHandler = () => setVisible(false);
+  const { contract } = useContract(contractAddress);
 
-    const { contract } = useContract(contractAddress);
-
-    const transferNFT = async () => {
-        try {
-            await contract.nft.transfer(recipientAddress, nftId)
-            closeHandler();
-        } catch (err) {
-            console.log("NFT transfer failed", err)
-        }
+  const transferNFT = async () => {
+    try {
+      await contract.nft.transfer(recipientAddress, nftId);
+      closeHandler();
+    } catch (err) {
+      console.log("NFT transfer failed", err);
     }
+  };
 
-    return (
-        <div>
-            <Button bordered onPress={handler}>
+  return (
+    <div>
+      <Button bordered onPress={handler}>
+        Transfer
+      </Button>
+      <Modal closeButton open={visible} onClose={closeHandler}>
+        <Modal.Header>
+          <Text b size={18}>
+            Transfer your NFT
+          </Text>
+        </Modal.Header>
+        <Modal.Body>
+          <Text>Send your NFT here: </Text>
+          <Input
+            clearable
+            bordered
+            color="default"
+            size="md"
+            label-placeholder="Recipient Address..."
+            aria-label="recipient"
+            onChange={(e) => setRecipientAddress(e.target.value)}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Grid.Container gap={2} justify="center">
+            <Grid>
+              <Button auto flat bordered onPress={closeHandler}>
+                Cancel
+              </Button>
+            </Grid>
+            <Grid>
+              <Button auto onPress={transferNFT}>
                 Transfer
-            </Button>
-            <Modal
-                closeButton
-                open={visible}
-                onClose={closeHandler}
-            >
-                <Modal.Header>
-                    <Text b size={18}>
-                        Transfer your NFT
-                    </Text>
-                </Modal.Header>
-                <Modal.Body>
-                    <Text>Send your NFT here: </Text>
-                    <Input
-                        clearable
-                        bordered
-                        color="default"
-                        size="md"
-                        label-placeholder="Recipient Address..."
-                        aria-label='recipient'
-                        onChange={e => setRecipientAddress(e.target.value)}
-                    />
-                </Modal.Body>
-                <Modal.Footer>
-                    <Grid.Container gap={2} justify="center">
-                        <Grid>
-                            <Button auto flat bordered onPress={closeHandler}>
-                                Cancel
-                            </Button>
-                        </Grid>
-                        <Grid>
-                            <Button auto onPress={transferNFT}>
-                                Transfer
-                            </Button>
-                        </Grid>
-                    </Grid.Container>
-                </Modal.Footer>
-            </Modal>
-        </div>
-    );
+              </Button>
+            </Grid>
+          </Grid.Container>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    CHAINLINK_FEED: process.env.CHAINLINK_FEED,
+    MARKETPLACE_ADDRESS: process.env.MARKETPLACE_ADDRESS,
+    NFT_COLLECTION_ADDRESS: process.env.NFT_COLLECTION_ADDRESS,
+    OWNER: process.env.OWNER,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
