02cfa65f4d0e6b076e909f39eaee3963d71b0745
John Agbanusi <agbanusijohn@gmail.com>
2022-08-28 17:19:09 +0100

    add chainlink pricefeed and keepers

---
+++ src/helpers/pricefeed.js
@@ -0,0 +1,17 @@
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+
+export const getPriceInMatic = async (provider, priceInUsd) => {
+  const sdk = new ThirdwebSDK(provider);
+  const contract = await sdk.getContract(provider.env.CHAINLINK_FEED);
+  const price =
+    (await contract.call("latestAnswer")) / (await contract.call("decimals"));
+  return priceInUsd / price;
+};
+
+export const getPriceInUsd = async (provider, priceInMatic) => {
+  const sdk = new ThirdwebSDK(provider);
+  const contract = await sdk.getContract(provider.env.CHAINLINK_FEED);
+  const price =
+    (await contract.call("latestAnswer")) / (await contract.call("decimals"));
+  return priceInMatic * price;
+};
